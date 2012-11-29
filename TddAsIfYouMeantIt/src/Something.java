f161163485e09e5ca638e6b0e580bf0c1f8b1e58
Adi Bolboaca <adi_bolb123@yahoo.com>
2012-11-29 19:47:20 +0200

    Refactored the test related to 2 by one boad

--- TddAsIfYouMeantIt/src/Something.java
+++ TddAsIfYouMeantIt/src/Something.java
@@ -7,17 +7,15 @@ public class Something {
 
 	@Test
 	public void x_will_win_on_a_board_with_1_cell(){
-		assertEquals("X", determineWinnerForBoard("X"));
+		assertEquals("X", determineResultForBoard("X"));
 	}
 
-	private String determineWinnerForBoard(String board) {
+	private String determineResultForBoard(String board) {
 		return board;
 	}
 	
 	@Test
 	public void there_will_be_a_draw_on_a_two_by_one_board() {
-		String twoByOneBoardWithACrossAndACircle = "draw";
-		String winner = twoByOneBoardWithACrossAndACircle;
-		assertEquals(winner, twoByOneBoardWithACrossAndACircle);
+		assertEquals("draw", determineResultForBoard("draw"));
 	}
 }
